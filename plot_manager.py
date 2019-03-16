e67b5d85559a7329e376c193660358d43f59f1e1
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-03-16 17:41:29 -0400

    Created a plot_manager to create the plot objects

---
+++ plot_manager.py
@@ -0,0 +1,48 @@
+import pyqtgraph as pg
+from general_function.create_cmap import create_cmap
+from data import Data
+from COLOR import *
+
+
+class PlotManager:
+    def __init__(self):
+        # date_axis = DateAxis(orientation='bottom')
+        # vb = pg.ViewBox()
+        # p = pg.PlotWidget(
+        #         viewBox=vb, axisItems={'bottom': date_axis},
+        #         enableMenu=False, title='Plot')
+        # Create the data object by reading the data from file
+        self.data = Data()
+        self.data.print_data()
+        self.pw = pg.PlotWidget()
+        self.pw.showGrid(x=True, y=True, alpha=0.5)
+        self.pw.setYRange(4, 14)
+
+    def plot_n_sleep_hours(self):
+        # Plot
+        self.plot_info(self.data.time_wake_up, pen_color='b')
+        self.plot_info(self.data.time_sleep, pen_color=(255, 165, 0))
+        self.plot_w_or_wo_alarm()
+
+    def plot_n_working_h(self):
+        self.pw.plot(
+            x=self.data.t, y=self.data.working_hours, label='Working hours', pen='g')
+        # dates = [dt(2018, 1, 5), dt(2018, 3, 6)]  # Date is given by the number of second after 1970
+        # y = [1, 6]
+        # self.pw.plot(x=dates, y=y)
+
+    def plot_info(self, info_to_plot, pen_color):
+        cmap = create_cmap(self.data.sleepines_scale)
+        sleepiness_color = [pg.mkBrush(color) for color in cmap]
+        self.pw.plot(
+            self.data.t, info_to_plot, symbol='o', symbolSize=8,
+            symbolBrush=sleepiness_color, label='Time wake up',
+            pen=pen_color)
+
+    def plot_w_or_wo_alarm(self):
+        color = [g, y, r, k]
+        self.data.w_or_wo_alarm[self.data.w_or_wo_alarm == -1] = 3
+        for i, val in enumerate(self.data.w_or_wo_alarm):
+            pen = pg.mkPen(color[int(val)], width=6)
+            v_line = pg.InfiniteLine(i, pen=pen)
+            self.pw.addItem(v_line)
--- tabs/sleep_and_work_tab.py
+++ tabs/sleep_and_work_tab.py
@@ -1,17 +1,13 @@
-import matplotlib.pyplot as plt
-import numpy as np
 # GUI
 from PyQt5.QtWidgets import *
 import pyqtgraph as pg
-# import sys
 from datetime import datetime as dt
 # -- My Packages --
 from general_function.inner_dock import InnerDock
 from pyqtgraph.dockarea import DockArea
-from general_function.create_cmap import create_cmap
 from app.pyqt_frequently_used import create_new_data_txt_box
-from data import Data
 from date_axis import DateAxis
+from plot_manager import PlotManager
 
 
 class SleepAndWorkTab(QWidget):
@@ -25,14 +21,17 @@ class SleepAndWorkTab(QWidget):
 
         self.set_pg_configuration()
         self.dock_area = self.create_dock_area()
-        self.pw = self.init_plot()
+        # Plot
+        self.plot_manager = PlotManager()
+        self.plot_manager.plot_n_sleep_hours()
+        self.plot_manager.plot_n_working_h()
 
-        self.add_info_to_layout()
-        # self.init_data_from_file()
-        self.data = Data()
-        self.data.print_data()
-        self.plot_n_sleep_hours()
-        self.plot_n_working_h()
+        plot_dock = InnerDock(
+            self.layout, 'show plot', b_pos=(0, 1), toggle_button=True)
+        plot_dock.layout.addWidget(self.plot_manager.pw)
+        self.dock_area.addDock(plot_dock.dock)
+
+        self.add_txt_box_to_layout()
 
     def create_tab(self):
         layout = QHBoxLayout(self)
@@ -50,22 +49,7 @@ class SleepAndWorkTab(QWidget):
         self.layout.addWidget(dock_area, 1, 0, 1, 6)
         return dock_area
 
-    def init_plot(self):
-        # date_axis = DateAxis(orientation='bottom')
-        # vb = pg.ViewBox()
-        # p = pg.PlotWidget(
-        #         viewBox=vb, axisItems={'bottom': date_axis},
-        #         enableMenu=False, title='Plot')
-        p = pg.PlotWidget()
-        p.showGrid(x=True, y=True, alpha=0.5)
-        p.setYRange(4, 14)
-        plot_dock = InnerDock(
-                self.layout, 'show plot', b_pos=(0, 1), toggle_button=True)
-        plot_dock.layout.addWidget(p)
-        self.dock_area.addDock(plot_dock.dock)
-        return p
-
-    def add_info_to_layout(self):
+    def add_txt_box_to_layout(self):
         # create a Inner dock for the adding of new information for the day
         settings_d = InnerDock(
                 self.layout, 'add info', b_pos=(0, 0), toggle_button=True,
@@ -78,37 +62,3 @@ class SleepAndWorkTab(QWidget):
         settings_d.layout.addWidget(add_info_b, 12, 0)
 
         self.dock_area.addDock(settings_d.dock, position='left')
-
-    def plot_n_sleep_hours(self):
-        # Plot
-        self.plot_info(self.data.time_wake_up, pen_color='b')
-        self.plot_info(self.data.time_sleep, pen_color=(255, 165, 0))
-        self.plot_w_or_wo_alarm()
-
-    def plot_n_working_h(self):
-        self.pw.plot(
-                x=self.data.t, y=self.data.working_hours, label='Working hours', pen='g')
-        # dates = [dt(2018, 1, 5), dt(2018, 3, 6)]  # Date is given by the number of second after 1970
-        # y = [1, 6]
-        # self.pw.plot(x=dates, y=y)
-
-    def plot_info(self, info_to_plot, pen_color):
-        cmap = create_cmap(self.data.sleepines_scale)
-        sleepiness_color = [pg.mkBrush(color) for color in cmap]
-        self.pw.plot(
-                self.data.t, info_to_plot, symbol='o', symbolSize=8,
-                symbolBrush=sleepiness_color, label='Time wake up',
-                pen=pen_color)
-
-    def plot_w_or_wo_alarm(self):
-        r = (255, 0, 0, 20)  # Put these color in a COLOR file (And,, create namedtuple instead ?)
-        g = (0, 255, 0, 20)
-        y = (229, 223, 0, 50)
-        k = (0, 0, 0, 10)
-        color = [g, y, r, k]
-        self.data.w_or_wo_alarm[self.data.w_or_wo_alarm == -1] = 3
-        for i, val in enumerate(self.data.w_or_wo_alarm):
-            pen = pg.mkPen(color[int(val)], width=6)
-            v_line = pg.InfiniteLine(i, pen=pen)
-            self.pw.addItem(v_line)
-
